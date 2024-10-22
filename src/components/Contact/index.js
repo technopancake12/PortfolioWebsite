a5c176fe7d27fe1d4978ce6e7a59885a228d6f46
Ian Bundy-Weiss <32020510+technopancake12@users.noreply.github.com>
2024-10-22 12:45:34 -0700

    fix contact me page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,26 +1,22 @@
-import emailjs from '@emailjs/browser'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
-import { useRef, useState } from 'react'
-import { useEffect } from 'react'
-import Loader from 'react-loaders'
-import PhotoF from '../../../src/assets/images/photo-face.png'
+import emailjs from '@emailjs/browser';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { useRef, useState, useEffect } from 'react';
+import Loader from 'react-loaders';
+import PhotoF from '../../../src/assets/images/photo-face.png';
 
 const Contact = () => {
-
-    const [letterClass, setLetterClass] = useState('text-animate')
-
-    const form = useRef()
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const form = useRef();
 
     useEffect(() => {
         setTimeout(() => {
-           return setLetterClass('text-animate-hover')
-        }, 3000)
-    })
+            setLetterClass('text-animate-hover');
+        }, 3000);
+    }, []);
 
     const sendEmail = (e) => {
         e.preventDefault();
-
         emailjs
             .sendForm(
                 'service_8go030z',
@@ -30,46 +26,45 @@ const Contact = () => {
             )
             .then(
                 () => {
-                    alert('Message Successfully Sent!')
-                    window.location.reload(false)
+                    alert('Message Successfully Sent!');
+                    window.location.reload(false);
                 },
                 () => {
-                    alert('Failed to send message, please try again')
+                    alert('Failed to send message, please try again');
                 }
-            )
-    }
- 
+            );
+    };
+
     return (
         <>
             <div className='contact-page'>
                 <div className='text-zone'>
                     <h1>
                         <AnimatedLetters 
-                        letterClass={letterClass}
-                        strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
-                        idx={15}/>
+                            letterClass={letterClass}
+                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+                            idx={15} />
                     </h1>
                     <p>
                         I am interested and looking for internships and jobs related to Data analyst, Data management, Web Developing, and Cyber Security, but anything that can help me sharpen my programming skills would be great!
-                        
                     </p>
                     <div className='contact-form'>
                         <form ref={form} onSubmit={sendEmail} netlify>
                             <ul>
                                 <li className='half'>
-                                    <input type='text' name='name' placeholder='Name' />
+                                    <input type='text' name='name' placeholder='Name' required />
                                 </li>
                                 <li className='half'>
-                                    <input type='email' name='from_email' placeholder='Email' />
+                                    <input type='email' name='from_email' placeholder='Email' required />
                                 </li>
                                 <li>
-                                    <input type='text' name='subject' placeholder='Subject' required/>
+                                    <input type='text' name='subject' placeholder='Subject' required />
                                 </li>
                                 <li>
-                                    <textarea placeholder='Message' name='message' required/>
+                                    <textarea placeholder='Message' name='message' required />
                                 </li>
                                 <li>
-                                    <input type='submit' className='flat-button' value="SEND"/>
+                                    <input type='submit' className='flat-button' value="SEND" />
                                 </li>
                             </ul>
                         </form>
@@ -77,11 +72,11 @@ const Contact = () => {
                 </div>
             </div>
             <div className='photo'>
-                <img className='photo-face' src={PhotoF} />
+                    <img className='photo-face' src={PhotoF} alt="Your face" />
             </div>
-            <Loader type='pacman'></Loader>
+            
         </>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
